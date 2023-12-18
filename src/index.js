8cd543738b8a0c8a0c27f195d15075d7b50f7aa7
TylerCharlton <TyCharlton1993@gmail.com>
2023-12-18 17:06:25 -0500

    Completed Lab

--- src/index.js
+++ src/index.js
@@ -1,38 +1,3 @@
-/* document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-  const form = document.querySelector("#create-task-form");
-  const input = document.querySelector("#new-task-description")
-  const list_el = document.querySelector("#tasks")
-
-
-  form.addEventListener('sumbit', (e) => {
-      e.preventDefault();
-
-      const task = input.ariaValueMax;
-
-      if (!task) {
-        alert("Please fill out the task");
-        return;
-      }
-
-      const task_el = document.createElement("div");
-      task_el.classList.add("task");
-
-      const task_content_el = document.createElement("div");
-      task_el.classList.add("content");
-      task_content_el.innerText = task;
-
-      task.length.appendChild(task_content_el);
-
-      list_el.appendChild(task_el)
-
-       
-  })
-
-   
-
-})
-*/
 
 const h1 = document.querySelector('h1')
 
@@ -47,16 +12,14 @@ h1.addEventListener('mouseover', changeColor)
 h1.addEventListener('mouseout', () => h1.style.color = "black")
 
 document.addEventListener('DOMContentLoaded', function () {
-
-  const form = document.getElementById('create-task-form');
-  const tasksList = document.getElementById('tasks');
-  
+  const form = document.querySelector('#create-task-form');
+  const tasksList = document.querySelector('#tasks');
 
   form.addEventListener('submit', function (event) {
     event.preventDefault();
 
     
-    const taskDescription = document.getElementById('new-task-description').value;
+    const taskDescription = document.querySelector('#new-task-description').value;
 
     
     if (taskDescription.trim() !== '') {
@@ -68,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
       console.log('New Task:', taskDescription);
 
-      document.getElementById('new-task-description').value = '';
+      document.querySelector('#new-task-description').value = '';
       console.log('')
     }
 
